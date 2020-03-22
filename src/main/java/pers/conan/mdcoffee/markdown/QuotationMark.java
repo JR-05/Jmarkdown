5678a573649db8a37015a8a812fc3e2a005c61fb
Conan Jordan <bryantconan@yahoo.co.jp>
2020-03-22 20:13:03 +0800

    编写引用标记

---
+++ src/main/java/pers/conan/mdcoffee/markdown/QuotationMark.java
@@ -0,0 +1,47 @@
+package pers.conan.mdcoffee.markdown;
+
+import pers.conan.mdcoffee.text.MarkDown;
+import pers.conan.mdcoffee.util.MarkUtil;
+
+/**
+ * 引用标记
+ * @author Conan
+ *
+ */
+public class QuotationMark extends BaseMark {
+    
+    /**
+     * 给引用标记添加一行内容
+     * @param line
+     */
+    public void put(String line) {
+        this.put(new TextMark(line));
+    }
+
+    @Override
+    public void mark() {
+        
+        /*
+         * 引用标记在标记时，
+         * 先解析内含标记，
+         * 然后在解析出来的每一行文本前加上'> '字符
+         */
+        String strInclusion = this.markInclusions();  // 获取标记后的内含标记文本内容
+        String[] markInclusions = strInclusion.split(String.valueOf(MarkDown.NEXT));  // 获取标记后的内含标记文本内容拆分出来的字符串数组
+        
+        StringBuilder quotation = new StringBuilder();
+        for (int i = 0; i < markInclusions.length; i ++) {
+            if (MarkUtil.isEmpty(markInclusions[i])) {  // 标记后的文本内容是空的话
+                continue;  // 继续下一次循环
+            }
+            if (i == 0) {
+                quotation.append(MarkDown.LEFT_AB + MarkDown.SPACE + markInclusions[i]);
+            } else {
+                quotation.append(MarkDown.NEXT + MarkDown.LEFT_AB + MarkDown.SPACE + markInclusions[i]);
+            }
+        }
+        
+        this.translated = quotation.toString();
+    }
+
+}
