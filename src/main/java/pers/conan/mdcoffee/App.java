2851fa6038806cc16cd57fb0ef91d7208c59224c
Conan Jordan <bryantconan@yahoo.co.jp>
2020-03-22 08:50:55 +0800

    Initialize project.

---
+++ src/main/java/pers/conan/mdcoffee/App.java
@@ -0,0 +1,13 @@
+package pers.conan.mdcoffee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
